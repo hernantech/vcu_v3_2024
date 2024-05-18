463d2c89d00a0519ab5cfef6a45bb0a1b0a58a32
hernantech <58359753+hernantech@users.noreply.github.com>
2024-05-18 11:45:09 -0700

    init

---
+++ Core/Inc/CAN_receiver.h
@@ -0,0 +1,15 @@
+/*
+ * CAN_receiver.h
+ *
+ *  Created on: May 14, 2024
+ *      Author: alexhernandez
+ */
+
+#ifndef SRC_CAN_RECEIVER_H_
+#define SRC_CAN_RECEIVER_H_
+
+CAN_INIT(){}
+
+
+
+#endif /* SRC_CAN_RECEIVER_H_ */
---
+++ Core/Inc/application.h
@@ -0,0 +1,20 @@
+/*
+ * application.h
+ *
+ *  Created on: May 14, 2024
+ *      Author: alexhernandez
+ */
+
+#import "stm32f7xx_hal_can.h"
+
+#ifndef INC_APPLICATION_H_
+#define INC_APPLICATION_H_
+
+
+
+#endif /* INC_APPLICATION_H_ */
+
+
+void app_tick(){};
+
+void app_init(){};
